f8cb496acd778a39630bcd236dc98658acb8d9b0
Zang Eric <ezang@lcs-mba3154.lcs.on.ca>
2018-01-13 10:17:06 -0500

    Initial Commit

---
+++ Guessing Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Guessing Game
+//
+//  Created by Zang, Eric on 2018-01-13.
+//  Copyright © 2018 Zang, Eric. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
